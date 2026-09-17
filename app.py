d159830a4d0cbbe96ebe9c38dab77247a611b6d9
John <theminor@gmail.com>
2026-09-17 12:15:48 -0400

    Pass app as import string to uvicorn so reload works without a warning

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ app.mount('/static', StaticFiles(directory=STATIC_DIR), name='static')
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8000, reload=True)
+    uvicorn.run('app:app', host='127.0.0.1', port=8000, reload=True)
